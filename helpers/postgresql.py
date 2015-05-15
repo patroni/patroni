0b669e0d20126462cf23cd1bf0ef943b6f00025f
Feike Steenbergen <feikesteenbergen@gmail.com>
2015-05-15 13:52:20 +0200

    Cleanup
    Removed aws reference from Dockerfile, remove unnecessary steps.
    Connect using libpq parameters, therefore providing nice application name for Governor.

--- helpers/postgresql.py
+++ helpers/postgresql.py
@@ -56,7 +56,7 @@ class Postgresql:
 
     def cursor(self):
         if not self.cursor_holder:
-            self.conn = psycopg2.connect('postgres://{}/postgres'.format(self.config['connect_address']))
+            self.conn = psycopg2.connect(**self.libpq_parameters)
             self.conn.autocommit = True
             self.cursor_holder = self.conn.cursor()
 
