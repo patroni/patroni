b8ce6794de1b2d43b0b572c0fa6c5310821aeae4
Feike Steenbergen <feikesteenbergen@gmail.com>
2015-05-18 13:35:43 +0200

    Initialize cluster with encoding=UTF8 by default.

--- helpers/postgresql.py
+++ helpers/postgresql.py
@@ -88,7 +88,7 @@ class Postgresql:
         return not os.path.exists(self.data_dir) or os.listdir(self.data_dir) == []
 
     def initialize(self):
-        if os.system(self._pg_ctl + ' initdb') == 0:
+        if os.system(self._pg_ctl + ' initdb -o --encoding=UTF8') == 0:
             self.write_pg_hba()
 
             return True
