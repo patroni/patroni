ebb8bce0f60277338043a2fa6b3c492faf8cbc90
Alexander Kukushkin <alexander.kukushkin@zalando.de>
2015-05-19 12:49:53 +0200

    some simple tests

---
+++ setup.py
@@ -0,0 +1,161 @@
+#!/usr/bin/env python
+
+"""
+    Setup file for governor
+"""
+
+import sys
+import os
+import inspect
+
+import setuptools
+from setuptools.command.test import test as TestCommand
+from setuptools import setup
+
+if sys.version_info < (2, 6, 0):
+    sys.stderr.write('FATAL: governor needs to be run with Python 2.6+\n')
+    sys.exit(1)
+
+__location__ = os.path.join(os.getcwd(), os.path.dirname(inspect.getfile(inspect.currentframe())))
+
+
+NAME = 'governor'
+MAIN_PACKAGE = 'governor.py'
+HELPERS = 'helpers'
+VERSION = '0.1'
+DESCRIPTION = 'A Template for PostgreSQL HA with etcd'
+LICENSE = 'The MIT License'
+URL = 'https://github.com/zalando/governor'
+AUTHOR = 'Alexander Kukushkin'
+EMAIL = 'alexander.kukushkins@zalando.de'
+
+COVERAGE_XML = True
+COVERAGE_HTML = False
+JUNIT_XML = True
+
+# Add here all kinds of additional classifiers as defined under
+# https://pypi.python.org/pypi?%3Aaction=list_classifiers
+CLASSIFIERS = [
+    'Development Status :: 4 - Beta',
+    'Environment :: Console',
+    'Intended Audience :: Developers',
+    'Intended Audience :: System Administrators',
+    'License :: OSI Approved :: The MIT License',
+    'Operating System :: POSIX :: Linux',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: Implementation :: CPython',
+]
+
+CONSOLE_SCRIPTS = ['governor = governor:main']
+
+
+class PyTest(TestCommand):
+
+    user_options = [('cov=', None, 'Run coverage'), ('cov-xml=', None, 'Generate junit xml report'), ('cov-html=',
+                    None, 'Generate junit html report'), ('junitxml=', None, 'Generate xml of test results')]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.cov_xml = False
+        self.cov_html = False
+        self.junitxml = None
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        if self.cov_xml or self.cov_html:
+            self.cov = ['--cov', MAIN_PACKAGE, '--cov', HELPERS, '--cov-report', 'term-missing']
+            if self.cov_xml:
+                self.cov.extend(['--cov-report', 'xml'])
+            if self.cov_html:
+                self.cov.extend(['--cov-report', 'html'])
+        if self.junitxml is not None:
+            self.junitxml = ['--junitxml', self.junitxml]
+
+    def run_tests(self):
+        try:
+            import pytest
+        except:
+            raise RuntimeError('py.test is not installed, run: pip install pytest')
+        params = {'args': self.test_args}
+        if self.cov:
+            params['args'] += self.cov
+            params['plugins'] = ['cov']
+        if self.junitxml:
+            params['args'] += self.junitxml
+        params['args'] += ['--doctest-modules', HELPERS, '-s']
+        errno = pytest.main(**params)
+        sys.exit(errno)
+
+
+def get_install_requirements(path):
+    content = open(os.path.join(__location__, path)).read()
+    return [req for req in content.split('\n') if req != '']
+
+
+def read(fname):
+    return open(os.path.join(__location__, fname)).read()
+
+
+def check_deps(deps):
+    '''check dependency licenses'''
+    from pkg_resources import Requirement
+    import requests
+    for dep in deps:
+        dep = Requirement.parse(dep)
+        url = 'https://pypi.python.org/pypi/{}/json'.format(dep.project_name)
+        r = requests.get(url)
+        data = r.json()
+        print(data['info'].get('name'), data['info'].get('license'))
+
+
+def setup_package():
+    # Assemble additional setup commands
+    cmdclass = {}
+    cmdclass['test'] = PyTest
+
+    # Some helper variables
+    version = os.getenv('GO_PIPELINE_LABEL', VERSION)
+
+    install_reqs = get_install_requirements('requirements.txt')
+
+    # check_deps(install_reqs)
+
+    command_options = {'test': {'test_suite': ('setup.py', 'tests')}}
+    if JUNIT_XML:
+        command_options['test']['junitxml'] = 'setup.py', 'junit.xml'
+    if COVERAGE_XML:
+        command_options['test']['cov_xml'] = 'setup.py', True
+    if COVERAGE_HTML:
+        command_options['test']['cov_html'] = 'setup.py', True
+
+    setup(
+        name=NAME,
+        version=version,
+        url=URL,
+        description=DESCRIPTION,
+        author=AUTHOR,
+        author_email=EMAIL,
+        license=LICENSE,
+        keywords='aws docker ec2 elb lb boto deployment route53 stack traffic',
+        long_description=read('README.md'),
+        classifiers=CLASSIFIERS,
+        test_suite='tests',
+        packages=setuptools.find_packages(exclude=['tests', 'tests.*']),
+        package_data={MAIN_PACKAGE: ["*.json"]},
+        install_requires=install_reqs,
+        setup_requires=['six', 'flake8'],
+        cmdclass=cmdclass,
+        tests_require=['pytest-cov', 'pytest'],
+        command_options=command_options,
+        entry_points={'console_scripts': CONSOLE_SCRIPTS},
+    )
+
+
+if __name__ == '__main__':
+    setup_package()
---
+++ tests/test_postgresql.py
@@ -0,0 +1,146 @@
+import os
+import psycopg2
+import unittest
+import shutil
+
+from helpers.etcd import Cluster, Member
+from helpers.postgresql import Postgresql
+
+
+def os_system(cmd):
+    return 0
+
+
+class MockCursor:
+
+    def __init__(self):
+        self.current = 0
+        self.results = []
+
+    def execute(self, sql, *params):
+        if sql.startswith('blabla'):
+            raise psycopg2.OperationalError()
+        elif sql.startswith('SELECT slot_name'):
+            self.results = [('blabla'), ('foobar')]
+        elif sql.startswith('SELECT pg_current_xlog_location()'):
+            self.results = [(0,)]
+        elif sql.startswith('SELECT %s - (pg_last_xlog_replay_location()'):
+            self.results = [(0,)]
+        elif sql.startswith('SELECT pg_last_xlog_replay_location()'):
+            self.results = [(0,)]
+        elif sql.startswith('SELECT pg_is_in_recovery()'):
+            self.results = [(False, )]
+        else:
+            self.results = []
+
+    def fetchone(self):
+        return self.results[0]
+
+    def close(self):
+        pass
+
+    def __iter__(self):
+        for i in self.results:
+            yield i
+
+
+class MockConnect:
+
+    def __init__(self):
+        self.autocommit = False
+
+    def cursor(self):
+        return MockCursor()
+
+    def close(self):
+        if not self.autocommit:
+            raise psycopg2.OperationalError()
+
+
+def psycopg2_connect(*args, **kwargs):
+
+    return MockConnect()
+
+
+def is_running():
+    return False
+
+
+class TestPostgresql(unittest.TestCase):
+
+    def __init__(self, method_name='runTest'):
+        self.setUp = self.set_up
+        self.tearDown = self.tear_down
+        super(TestPostgresql, self).__init__(method_name)
+
+    def set_up(self):
+        os.system = os_system
+        self.p = Postgresql({'name': 'test0', 'data_dir': 'data/test0', 'listen': '127.0.0.1, 127.0.0.2:5432', 'connect_address': '127.0.0.2:5432', 'replication': {
+                            'username': 'replicator', 'password': 'rep-pass', 'network': '127.0.0.1/32'}, 'parameters': {'foo': 'bar'}, 'recovery_conf': {'foo': 'bar'}})
+        psycopg2.connect = psycopg2_connect
+        if not os.path.exists(self.p.data_dir):
+            os.makedirs(self.p.data_dir)
+        self.leader = Member('leader', 'postgres://replicator:rep-pass@127.0.0.1:5434/postgres', 28)
+
+    def tear_down(self):
+        shutil.rmtree('data')
+
+    def test_data_directory_empty(self):
+        self.assertTrue(self.p.data_directory_empty())
+
+    def test_initialize(self):
+        self.assertTrue(self.p.initialize())
+        self.assertTrue(os.path.exists(os.path.join(self.p.data_dir, 'pg_hba.conf')))
+
+    def test_start(self):
+        self.assertFalse(self.p.start())
+        self.p.is_running = is_running
+        with open(os.path.join(self.p.data_dir, 'postmaster.pid'), 'w'):
+            pass
+        self.assertTrue(self.p.start())
+
+    def test_sync_from_leader(self):
+        self.assertTrue(self.p.sync_from_leader(self.leader))
+
+    def test_follow_the_leader(self):
+        self.p.demote(self.leader)
+        self.p.follow_the_leader(None)
+        self.p.demote(self.leader)
+        self.p.follow_the_leader(self.leader)
+        self.p.follow_the_leader(Member('leader', 'postgres://replicator:rep-pass@127.0.0.1:5435/postgres', 28))
+
+    def test_create_replication_slots(self):
+        self.p.start()
+        self.p.create_replication_slots('qaz')
+
+    def test_query(self):
+        self.p.query('select 1')
+        self.p.conn.autocommit = False
+        self.assertRaises(psycopg2.OperationalError, self.p.query, 'blabla')
+        self.p.query('select %s', 1)
+
+    def test_is_healthiest_node(self):
+        leader = Member('leader', 'postgres://replicator:rep-pass@127.0.0.1:5435/postgres', 28)
+        me = Member('test0', 'postgres://replicator:rep-pass@127.0.0.1:5434/postgres', 28)
+        other = Member('test1', 'postgres://replicator:rep-pass@127.0.0.1:5433/postgres', 28)
+        cluster = Cluster(leader, 0, [leader, me, other])
+        self.assertTrue(self.p.is_healthiest_node(cluster))
+        self.p.config['maximum_lag_on_failover'] = -1
+        self.assertFalse(self.p.is_healthiest_node(cluster))
+
+    def test_is_leader(self):
+        self.assertTrue(self.p.is_leader())
+
+    def test_reload(self):
+        self.assertTrue(self.p.reload())
+
+    def test_is_healthy(self):
+        self.assertTrue(self.p.is_healthy())
+        self.p.is_running = is_running
+        self.assertFalse(self.p.is_healthy())
+
+    def test_promote(self):
+        self.assertTrue(self.p.promote())
+
+    def test_last_operation(self):
+        self.assertEquals(self.p.last_operation(), 0)
