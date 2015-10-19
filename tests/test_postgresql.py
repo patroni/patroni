8f606e4ff9a85f6f6d76db744a48880ec940f827
Oleksii Kliukin <oleksii.kliukin@zalando.de>
2015-10-19 15:13:24 +0200

    Add a missing call to restore_configuration_files.
    I accidentially removed the call when moving the backup functions
    to the external script. It is intended to save the configuration,
    so that at the restore phase one can just copy backup files.
    Its primary intention was to save configuration files in the WAL-E
    case (WAL-E just omits everything with .conf), but it is also
    useful in the pg_basebackup case, which omits all symlinks, leaving
    the cluster with .conf files symlinked in the broken state.

--- tests/test_postgresql.py
+++ tests/test_postgresql.py
@@ -19,6 +19,11 @@ from test_ha import false
 import subprocess
 
 
+def is_file_raise_on_backup(*args, **kwargs):
+    if args[0].endswith('.backup'):
+        raise Exception("foo")
+
+
 class MockCursor:
 
     def __init__(self, connection):
@@ -429,3 +434,15 @@ class TestPostgresql(unittest.TestCase):
         self.p.cleanup_archive_status()
         mock_unlink.assert_not_called()
         mock_remove.assert_not_called()
+
+    @patch('os.path.isfile', MagicMock(return_value=True))
+    @patch('shutil.copy', side_effect=Exception)
+    def test_save_configuration_files(self, mock_copy):
+        shutil.copy = mock_copy
+        self.p.save_configuration_files()
+
+    @patch('os.path.isfile', MagicMock(side_effect=is_file_raise_on_backup))
+    @patch('shutil.copy', side_effect=Exception)
+    def test_restore_configuration_files(self, mock_copy):
+        shutil.copy = mock_copy
+        self.p.restore_configuration_files()
