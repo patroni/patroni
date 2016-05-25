89adc0717a8653bbef72aba36faf0e9fb345f01f
Alexander Kukushkin <alexander.kukushkin@zalando.de>
2016-05-25 15:02:42 +0200

    Set loglevel back to INFO

--- patroni/__init__.py
+++ patroni/__init__.py
@@ -112,7 +112,7 @@ class Patroni(object):
 
 
 def main():
-    logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', level=logging.DEBUG)
+    logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', level=logging.INFO)
     logging.getLogger('requests').setLevel(logging.WARNING)
     setup_signal_handlers()
 
