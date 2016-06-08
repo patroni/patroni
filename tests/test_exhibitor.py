50d118c3aa126afedbf0ff4cdc2337cf017ef449
Alexander Kukushkin <alexander.kukushkin@zalando.de>
2016-06-08 12:00:28 +0200

    Split ZooKeeper and Exhibitor
    Originally Exhibitor was supported in the ZooKeeper class and
    configuration for Exhibitor was taken also from `zookeeper` section in
    the yaml config file. In fact, Exhibitor just extends ZooKeeper and now
    it is reflected in the code and also Exhibitor got it's own section in
    the config.yaml file. It will make it easier to configure Exhibitor
    hosts and port via environment variables when PR#211 will be merged.

---
+++ tests/test_exhibitor.py
@@ -0,0 +1,30 @@
+import unittest
+
+from mock import Mock, patch
+from patroni.dcs.exhibitor import ExhibitorEnsembleProvider, Exhibitor
+from patroni.dcs.zookeeper import ZooKeeperError
+from test_etcd import SleepException, requests_get
+from test_zookeeper import MockKazooClient
+
+
+@patch('requests.get', requests_get)
+@patch('time.sleep', Mock(side_effect=SleepException))
+class TestExhibitorEnsembleProvider(unittest.TestCase):
+
+    def test_init(self):
+        self.assertRaises(SleepException, ExhibitorEnsembleProvider, ['localhost'], 8181)
+
+    def test_poll(self):
+        self.assertFalse(ExhibitorEnsembleProvider(['exhibitor'], 8181).poll())
+
+
+class TestExhibitor(unittest.TestCase):
+
+    @patch('requests.get', requests_get)
+    @patch('patroni.dcs.zookeeper.KazooClient', MockKazooClient)
+    def setUp(self):
+        self.e = Exhibitor('foo', {'hosts': ['localhost', 'exhibitor'], 'port': 8181, 'scope': 'test'})
+
+    @patch.object(ExhibitorEnsembleProvider, 'poll', Mock(return_value=True))
+    def test_get_cluster(self):
+        self.assertRaises(ZooKeeperError, self.e.get_cluster)
