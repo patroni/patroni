9fdd021e08cbc7f8a41a72e1ee1f518a485f2c35
Alexander Kukushkin <alexander.kukushkin@zalando.de>
2016-08-29 10:25:46 +0200

    Fix unit-tests for api

--- tests/test_api.py
+++ tests/test_api.py
@@ -40,10 +40,6 @@ class MockHa(object):
     state_handler = MockPostgresql()
 
     @staticmethod
-    def is_paused():
-        return False
-
-    @staticmethod
     def schedule_reinitialize():
         return 'reinitialize'
 
@@ -183,7 +179,9 @@ class TestRestApiHandler(unittest.TestCase):
             MockRestApiServer(RestApiHandler, 'POST /reload HTTP/1.0' + self._authorization)
         self.assertIsNotNone(MockRestApiServer(RestApiHandler, 'POST /reload HTTP/1.0' + self._authorization))
 
-    def test_do_POST_restart(self):
+    @patch.object(MockPatroni, 'dcs')
+    def test_do_POST_restart(self, mock_dcs):
+        mock_dcs.get_cluster.return_value.is_paused.return_value = False
         request = 'POST /restart HTTP/1.0' + self._authorization
         self.assertIsNotNone(MockRestApiServer(RestApiHandler, request))
 
@@ -225,9 +223,8 @@ class TestRestApiHandler(unittest.TestCase):
                 request = make_request(role='master', postgres_version='9.5.2')
                 MockRestApiServer(RestApiHandler, request)
 
-        with patch.object(MockHa, 'is_paused', Mock(return_value=True)):
-            request = make_request(schedule='2016-08-42 12:45TZ+1', role='master')
-            MockRestApiServer(RestApiHandler, request)
+        mock_dcs.get_cluster.return_value.is_paused.return_value = True
+        MockRestApiServer(RestApiHandler, make_request(schedule='2016-08-42 12:45TZ+1', role='master'))
 
     def test_do_DELETE_restart(self):
         for retval in (True, False):
@@ -236,8 +233,9 @@ class TestRestApiHandler(unittest.TestCase):
                 self.assertIsNotNone(MockRestApiServer(RestApiHandler, request))
 
     @patch.object(MockPatroni, 'dcs')
-    def test_do_POST_reinitialize(self, dcs):
-        cluster = dcs.get_cluster.return_value
+    def test_do_POST_reinitialize(self, mock_dcs):
+        cluster = mock_dcs.get_cluster.return_value
+        cluster.is_paused.return_value = False
         request = 'POST /reinitialize HTTP/1.0' + self._authorization
         MockRestApiServer(RestApiHandler, request)
         cluster.is_unlocked.return_value = False
@@ -247,8 +245,8 @@ class TestRestApiHandler(unittest.TestCase):
         cluster.leader.name = 'test'
         self.assertIsNotNone(MockRestApiServer(RestApiHandler, request))
 
-        with patch.object(MockHa, 'is_paused', Mock(return_value=True)):
-            self.assertIsNotNone(MockRestApiServer(RestApiHandler, request))
+        cluster.is_paused.return_value = True
+        self.assertIsNotNone(MockRestApiServer(RestApiHandler, request))
 
     @patch('time.sleep', Mock())
     def test_RestApiServer_query(self):
@@ -315,9 +313,6 @@ class TestRestApiHandler(unittest.TestCase):
         MockRestApiServer(RestApiHandler, request)
         with patch.object(MockPatroni, 'dcs') as d:
             d.manual_failover.return_value = False
-            MockRestApiServer(RestApiHandler, request)
-
-        with patch.object(MockHa, 'is_paused', Mock(return_value=True)):
             MockRestApiServer(RestApiHandler, request)
 
         # Exception: No timezone specified
