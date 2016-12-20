03893c12a48758120b2b1295b056f84f830867f8
Oleksii Kliukin <oleksii.kliukin@zalando.de>
2016-12-20 11:46:21 +0100

    Revert "Use RTD theme, since it's mobile friendly."
    The actual theme instructions are in the conf.py already and
    the theme is used on RTD by default.
    
    This reverts commit 1ce3dfb4f9363b13261f70897d3b495b1ce49378.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -87,9 +87,7 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-import sphinx_rtd_theme
-html_theme = "sphinx_rtd_theme"
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
