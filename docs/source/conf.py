320a6f34f65cb31cb0ad14fbdf28002016739f27
Oleksii Kliukin <oleksii.kliukin@zalando.de>
2016-12-20 11:25:35 +0100

    'Solve' the 'could not mport extensions sphinx.githubpages' complaint on readthedocs.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,8 +35,7 @@ extensions = ['sphinx.ext.intersphinx',
     'sphinx.ext.todo',
     'sphinx.ext.mathjax',
     'sphinx.ext.ifconfig',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages']
+    'sphinx.ext.viewcode']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
