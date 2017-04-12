f70a00a9123c9b5a5efca9864794e8a757959d0a
Ants Aasma <ants.aasma@eesti.ee>
2017-04-12 16:27:58 +0300

    Add --replace option to patronictl edit-config

--- patroni/ctl.py
+++ patroni/ctl.py
@@ -927,9 +927,10 @@ def invoke_editor(before_editing, data, cluster_name):
               help='Set specific PostgreSQL parameter value. Shorthand for -s postgresql.parameters.'
                    'Can be specified multiple times')
 @click.option('--apply', 'apply_filename', help='Apply configuration from file. Use - for stdin.')
+@click.option('--replace', 'replace_filename', help='Apply configuration from file replacing existing configuration. Use - for stdin.')
 @option_force
 @click.pass_obj
-def edit_config(obj, cluster_name, force, quiet, kvpairs, pgkvpairs, apply_filename):
+def edit_config(obj, cluster_name, force, quiet, kvpairs, pgkvpairs, apply_filename, replace_filename):
     dcs = get_dcs(obj, cluster_name)
     cluster = dcs.get_cluster()
 
@@ -937,6 +938,9 @@ def edit_config(obj, cluster_name, force, quiet, kvpairs, pgkvpairs, apply_filen
 
     after_editing = None  # Serves as a flag if any changes were requested
     changed_data = cluster.config.data
+
+    if replace_filename:
+        after_editing, changed_data = apply_yaml_file({}, replace_filename)
 
     if apply_filename:
         after_editing, changed_data = apply_yaml_file(changed_data, apply_filename)
@@ -957,13 +961,14 @@ def edit_config(obj, cluster_name, force, quiet, kvpairs, pgkvpairs, apply_filen
     if not quiet:
         show_diff(before_editing, after_editing)
 
-    if apply_filename == '-' and not force:
+    if (apply_filename == '-' or replace_filename == '-') and not force:
         click.echo("Use --force option to apply changes")
         return
 
     if force or click.confirm('Apply these changes?'):
         if not dcs.set_config_value(json.dumps(changed_data), cluster.config.modify_index):
             raise PatroniCtlException("Config modification aborted due to concurrent changes")
+        click.echo("Configuration changed")
 
 
 @ctl.command('show-config', help="Show cluster configuration")
