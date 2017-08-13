b84d12dc714a8f85ee242eeaf2f358e6c92e83fa
Alexander Kukushkin <alexander.kukushkin@zalando.de>
2017-08-13 12:52:51 +0200

    Configure initdb

--- kubernetes/entrypoint.sh
+++ kubernetes/entrypoint.sh
@@ -5,6 +5,12 @@ bootstrap:
   dcs:
     postgresql:
       use_pg_rewind: true
+  initdb:
+  - auth-host: md5
+  - auth-local: trust
+  - encoding: UTF8
+  - locale: en_US.UTF-8
+  - data-checksums
   pg_hba:
   - host all all 0.0.0.0/0 md5
   - host replication ${PATRONI_REPLICATION_USERNAME} ${POD_IP}/16    md5
