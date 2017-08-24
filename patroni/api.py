23152a7fc4735ebdd7093b11525df150a3825d10
Alexander Kukushkin <cyberdemn@gmail.com>
2017-08-24 07:55:02 +0200

    synchronous_standby_names must be quoted with quote_ident (#505)
    in addition to that implement additional checks around manual failover and recover when synchronous_mode is enabled
    
    * Comparison must be case insensitive

--- patroni/api.py
+++ patroni/api.py
@@ -293,9 +293,15 @@ class RestApiHandler(BaseHTTPRequestHandler):
         if leader and (not cluster.leader or cluster.leader.name != leader):
             return 'leader name does not match'
         if candidate:
+            if cluster.is_synchronous_mode() and cluster.sync.sync_standby != candidate:
+                return 'candidate name does not match with sync_standby'
             members = [m for m in cluster.members if m.name == candidate]
             if not members:
                 return 'candidate does not exists'
+        elif cluster.is_synchronous_mode():
+            members = [m for m in cluster.members if m.name == cluster.sync.sync_standby]
+            if not members:
+                return 'failover is not possible: can not find sync_standby'
         else:
             members = [m for m in cluster.members if m.name != cluster.leader.name and m.api_url]
             if not members:
