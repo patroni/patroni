8f9ef90b90f0014e90c8babac9fe8a2a23ba0aea
ainlolcat <ainlolcat@gmail.com>
2017-11-07 20:11:09 +0400

    change pid detection logic

--- patroni/postgresql.py
+++ patroni/postgresql.py
@@ -760,8 +760,7 @@ class Postgresql(object):
                 self.postmaster = {'pid': pmpid, 'start_time': pmstart}
                 logger.info("Updated postmaster info: {}".format(self.postmaster))
             except ValueError:
-                logger.exception("Cannot update postmaster info with data duw garbage in pid file: {}".format(pid_data))
-                pass
+                logger.exception("Cannot update postmaster info with data due garbage in pid file: {}".format(pid_data))
         else:
             logger.warning("Cannot update postmaster info with data from pid file: {}".format(pid_data))
 
