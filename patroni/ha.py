953b433a55e0a57e1271f1b3cf3fc28ed8981df7
Ants Aasma <ants.aasma@eesti.ee>
2017-11-13 16:10:50 +0200

    If data dir goes away, stop in immediate mode

--- patroni/ha.py
+++ patroni/ha.py
@@ -1025,7 +1025,7 @@ class Ha(object):
             # is data directory empty?
             if self.state_handler.data_directory_empty():
                 self.state_handler.set_role('uninitialized')
-                self.state_handler.stop()
+                self.state_handler.stop('immediate')
                 # In case datadir went away while we were master. TODO: check for this and try to stop postgresql.
                 self.watchdog.disable()
 
