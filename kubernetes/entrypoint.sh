e3b431bf3897fdff5446c93a5c0e0dc5d7d1086c
Alexander Kukushkin <alexander.kukushkin@zalando.de>
2018-03-21 14:01:22 +0100

    Switch to endpoints and remove callback

--- kubernetes/entrypoint.sh
+++ kubernetes/entrypoint.sh
@@ -13,24 +13,11 @@ bootstrap:
   - data-checksums
   pg_hba:
   - host all all 0.0.0.0/0 md5
-  - host replication ${PATRONI_REPLICATION_USERNAME} ${POD_IP}/16    md5
+  - host replication ${PATRONI_REPLICATION_USERNAME} ${PATRONI_KUBERNETES_POD_IP}/16    md5
 restapi:
-  connect_address: '${POD_IP}:8008'
+  connect_address: '${PATRONI_KUBERNETES_POD_IP}:8008'
 postgresql:
-  connect_address: '${POD_IP}:5432'
-  authentication:
-    superuser:
-      password: '${PATRONI_SUPERUSER_PASSWORD}'
-    replication:
-      password: '${PATRONI_REPLICATION_PASSWORD}'
-  callbacks:
-    on_start: /callback.py
-    on_stop: /callback.py
-    on_role_change: /callback.py
+  connect_address: '${PATRONI_KUBERNETES_POD_IP}:5432'
 __EOF__
-
-unset PATRONI_SUPERUSER_PASSWORD PATRONI_REPLICATION_PASSWORD
-export KUBERNETES_NAMESPACE=$PATRONI_KUBERNETES_NAMESPACE
-export POD_NAME=$PATRONI_NAME
 
 exec /usr/bin/python /usr/local/bin/patroni /home/postgres/patroni.yml
