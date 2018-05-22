afdcb5b28b4736ba63a88553283482ad0cbc9b83
erthalion <9erthalion6@gmail.com>
2018-05-22 11:02:20 +0200

    Don't rely on the hardcoded port

--- features/steps/standby_cluster.py
+++ features/steps/standby_cluster.py
@@ -10,13 +10,14 @@ def start_patroni(context, name, cluster_name):
 
 @step('I start {name:w} in a standby cluster {cluster_name:w} as a clone of {name2:w}')
 def start_patroni_stanby_cluster(context, name, cluster_name, name2):
+    port = context.pctl._processes[name2]._connkwargs.get('port')
     return context.pctl.start(name, custom_config={
         "scope": cluster_name,
         "bootstrap": {
             "dcs": {
                 "standby_cluster" :{
                     "host": "localhost",
-                    "port": 5441,
+                    "port": port,
                     "primary_slot_name": "postgresql1",
                 }
             }
