7d32ee4380b7beac44c3ff24365b2103796f6888
erthalion <9erthalion6@gmail.com>
2018-05-24 14:54:19 +0200

    Config should be dict, not just not None

--- patroni/common_config.py
+++ patroni/common_config.py
@@ -2,7 +2,7 @@ def is_standby_cluster(config):
     """ Check whether or not provided configuration describes a standby cluster.
         Config can be both patroni config or cluster.config.data
     """
-    return config is not None and (
+    return isistance(config, dict) and (
         config.get('host') is not None or
         config.get('port') is not None or
         config.get('restore_command') is not None
