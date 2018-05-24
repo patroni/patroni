65b32cfdbfb1a273faf6400759dbc39a2691e304
erthalion <9erthalion6@gmail.com>
2018-05-24 15:40:42 +0200

    Fix typo

--- patroni/common_config.py
+++ patroni/common_config.py
@@ -2,7 +2,7 @@ def is_standby_cluster(config):
     """ Check whether or not provided configuration describes a standby cluster.
         Config can be both patroni config or cluster.config.data
     """
-    return isistance(config, dict) and (
+    return isinstance(config, dict) and (
         config.get('host') is not None or
         config.get('port') is not None or
         config.get('restore_command') is not None
