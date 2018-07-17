686754a6d2f8571a862ea1142614bb34c65bb816
Tony Sorrentino <4976173+tonys66@users.noreply.github.com>
2018-07-17 11:10:45 -0500

    Streamlined "stopped" status setting in shutdown
    Streamlined "stopped" status setting in shutdown to avoid calling self.state_handler.set_state to set status to "stopped" when it is already in that status.

--- patroni/ha.py
+++ patroni/ha.py
@@ -1168,7 +1168,6 @@ class Ha(object):
             if not self.state_handler.is_running():
                 if self.has_lock():
                     self.dcs.delete_leader()
-                self.state_handler.set_state('stopped')
                 self.touch_member()
             else:
                 # XXX: what about when Patroni is started as the wrong user that has access to the watchdog device
