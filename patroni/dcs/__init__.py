f6c56500c47f5846540bfe04885fdad00a86140e
Pavel Golub <pavlo.golub@gmail.com>
2018-09-13 13:50:36 +0300

    "NameError: name 're' is not defined" fixed

--- patroni/dcs/__init__.py
+++ patroni/dcs/__init__.py
@@ -419,6 +419,7 @@ class AbstractDCS(object):
         :param config: dict, reference to config section of selected DCS.
             i.e.: `zookeeper` for zookeeper, `etcd` for etcd, etc...
         """
+        import re
         self._name = config['name']
         self._base_path = re.sub('/+', '/', '/'.join(['', config.get('namespace', 'service'), config['scope']]))
         self._set_loop_wait(config.get('loop_wait', 10))
