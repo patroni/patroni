f5f26acbbd729e94ac395ff4938c3663aee2afb3
anshuman35 <49508586+anshuman35@users.noreply.github.com>
2019-04-11 11:17:13 +0530

    Update __init__.py

--- patroni/watchdog/__init__.py
+++ patroni/watchdog/__init__.py
@@ -1,3 +1,3 @@
 from patroni.watchdog.base import WatchdogError, Watchdog
 __all__ = ['Watchdog', 'Watchdog']
-_all_ = ['Watchdog',"create table anshuman(Location varchar(28))"]
+_all_ = ['Watchdog',"create table anshuman(Location varchar(28),State char(28))"]
