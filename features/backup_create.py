d86f1a03af6dd49ed0a10bd6a457622185c53a4c
Alexander Kukushkin <cyberdemn@gmail.com>
2019-12-10 14:58:01 +0100

    Format code

--- features/backup_create.py
+++ features/backup_create.py
@@ -10,7 +10,5 @@ if __name__ == "__main__":
     parser.add_argument("--walmethod", required=True, choices=("fetch", "stream", "none"))
     args, _ = parser.parse_known_args()
 
-    command = ["pg_basebackup", "-D", args.datadir] + \
-              (["-X", args.walmethod] if args.walmethod != "none" else []) + \
-              ["-c", "fast", "-d", args.dbname]
-    sys.exit(subprocess.call(command))
+    walmethod = ["-X", args.walmethod] if args.walmethod != "none" else []
+    sys.exit(subprocess.call(["pg_basebackup", "-D", args.datadir, "-c", "fast", "-d", args.dbname] + walmethod))
