6b9ff090fa19fa90c013953feb838f7cd36f5a89
Kostiantyn Nemchenko <kostiantyn.nemchenko@gmail.com>
2020-02-01 12:37:23 +0200

    Show tags for member via patronictl

--- patroni/ctl.py
+++ patroni/ctl.py
@@ -726,7 +726,7 @@ def output_members(cluster, name, extended=False, fmt='pretty'):
     cluster = cluster_as_json(cluster)
 
     columns = ['Cluster', 'Member', 'Host', 'Role', 'State', 'TL', 'Lag in MB']
-    for c in ('Pending restart', 'Scheduled restart'):
+    for c in ('Pending restart', 'Scheduled restart', 'Tags'):
         if extended or any(m.get(c.lower().replace(' ', '_')) for m in cluster['members']):
             columns.append(c)
 
@@ -741,7 +741,8 @@ def output_members(cluster, name, extended=False, fmt='pretty'):
         m.update(cluster=name, member=m['name'], tl=m.get('timeline', ''),
                  role='' if m['role'] == 'replica' else m['role'].replace('_', ' ').title(),
                  lag_in_mb=round(lag/1024/1024) if isinstance(lag, six.integer_types) else lag,
-                 pending_restart='*' if m.get('pending_restart') else '')
+                 pending_restart='*' if m.get('pending_restart') else '',
+                 tags=m.get('tags', ''))
 
         if append_port:
             m['host'] = ':'.join([m['host'], str(m['port'])])
