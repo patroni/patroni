1229cf2c16e4c46a410dad985b7262a326414e15
Alexander Kukushkin <cyberdemn@gmail.com>
2020-06-05 09:33:50 +0200

    Ignore hba_file and ident_file when they match with defaults (#1555)
    It is possible to specify custom hba_file and ident_file in the postgresql configuration parameters and Patroni is considering that these files are managed externally. It could happen that locations of these files matching with default locations of pg_hba,conf and pg_ident.conf. In this case we will ignore custom values and fallback to the default workflow, i.e. Patroni will overwrite them.
    
    Close: https://github.com/zalando/patroni/issues/1544

--- patroni/postgresql/bootstrap.py
+++ patroni/postgresql/bootstrap.py
@@ -363,7 +363,7 @@ END;$$""".format(f, rewind['username'])
                     # at this point there should be no recovery.conf
                     postgresql.config.remove_recovery_conf()
 
-                    if postgresql.config.hba_file and postgresql.config.hba_file != postgresql.config.pg_hba_conf:
+                    if postgresql.config.hba_file:
                         postgresql.restart()
                     else:
                         postgresql.config.replace_pg_hba()
