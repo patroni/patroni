8e046bed28b690866bd097ad8398d64f48c228e2
ksarabu1 <62157128+ksarabu1@users.noreply.github.com>
2020-06-24 09:59:17 -0400

    Update patroni/postgresql/__init__.py
    Co-authored-by: Alexander Kukushkin <cyberdemn@gmail.com>

--- patroni/postgresql/__init__.py
+++ patroni/postgresql/__init__.py
@@ -897,7 +897,7 @@ class Postgresql(object):
 
         :returns tuple of candidate name or None, and bool showing if the member is the active synchronous standby.
         """
-        current = [x.lower() for x in cluster.sync.members] if cluster.sync.members else []
+        current = [x.lower() for x in cluster.sync.members or []]
         members = {m.name.lower(): m for m in cluster.members}
         candidates = []
         # Pick candidates based on who has flushed WAL farthest.
