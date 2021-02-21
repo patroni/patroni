591d3848af7043621398cc9de756a95d2aa1d387
Mark Mercado <mamercad@gmail.com>
2021-02-21 09:24:21 -0500

    Deduping

--- patroni/api.py
+++ patroni/api.py
@@ -186,10 +186,7 @@ class RestApiHandler(BaseHTTPRequestHandler):
 
         metrics.append("# HELP patroni_running Value is 1 if Patroni/Postgres is up, 0 otherwise.")
         metrics.append("# TYPE patroni_running gauge")
-        if postgres['state'] == 'running':
-            metrics.append("patroni_running 1")
-        else:
-            metrics.append("patroni_running 0")
+        metrics.append("patroni_running {0}".format(int(postgres['state'] == 'running')))
 
         try:
             postmaster_epoch_start_time = int(
@@ -203,35 +200,22 @@ class RestApiHandler(BaseHTTPRequestHandler):
         except ValueError:
             pass
 
-        if postgres['role'] == 'master':
-            metrics.append("# HELP patroni_master Value is 1 if this node is the leader, 0 otherwise.")
-            metrics.append("# TYPE patroni_master gauge")
-            metrics.append("patroni_master 1")
-            metrics.append("# HELP patroni_xlog_location Current location of the Postgres transaction log, 0 if this node is not the leader.")  # noqa: E501
-            metrics.append("# TYPE patroni_xlog_location counter")
-            metrics.append("patroni_xlog_location {0}".format(postgres['xlog']['location']))
-        else:
-            metrics.append("# HELP patroni_master Value is 1 if this node is the leader, 0 otherwise.")
-            metrics.append("# TYPE patroni_master gauge")
-            metrics.append("patroni_master 0")
-            metrics.append("# HELP patroni_xlog_location Current location of the Postgres transaction log, 0 if this node is not the leader.")  # noqa: E501
-            metrics.append("# TYPE patroni_xlog_location counter")
-            metrics.append("patroni_xlog_location 0")
+        metrics.append("# HELP patroni_master Value is 1 if this node is the leader, 0 otherwise.")
+        metrics.append("# TYPE patroni_master gauge")
+        metrics.append("patroni_master {0}".format(int(postgres['role'] == 'master')))
+        metrics.append("# HELP patroni_xlog_location Current location of the PostgreSQL"
+                       " transaction log, 0 if this node is not the leader.")
+        metrics.append("# TYPE patroni_xlog_location counter")
+        metrics.append("patroni_xlog_location {0}".format(postgres.get('xlog', {}).get('location', 0)))
 
-        if postgres['role'] == 'replica':
-            metrics.append("# HELP patroni_replica Value is 1 if this node is a replica, 0 otherwise.")
-            metrics.append("# TYPE patroni_replica gauge")
-            metrics.append("patroni_replica 1")
-            metrics.append("# HELP patroni_xlog_received_location Current location of the received Postgres transaction log, 0 if this node is not a replica.")  # noqa: E501
-            metrics.append("# TYPE patroni_xlog_received_location counter")
-            metrics.append("patroni_xlog_received_location {0}".format(postgres['xlog']['received_location']))
-        else:
-            metrics.append("# HELP patroni_replica Value is 1 if this node is a replica, 0 otherwise.")
-            metrics.append("# TYPE patroni_replica gauge")
-            metrics.append("patroni_replica 0")
-            metrics.append("# HELP patroni_xlog_received_location Current location of the received Postgres transaction log, 0 if this node is not a replica.")  # noqa: E501
-            metrics.append("# TYPE patroni_xlog_received_location counter")
-            metrics.append("patroni_xlog_received_location 0")
+        metrics.append("# HELP patroni_replica Value is 1 if this node is a replica, 0 otherwise.")
+        metrics.append("# TYPE patroni_replica gauge")
+        metrics.append("patroni_replica 1")
+        metrics.append("# HELP patroni_xlog_received_location Current location of the received"
+                       " PostgreSQL transaction log, 0 if this node is not a replica.")
+        metrics.append("# TYPE patroni_xlog_received_location counter")
+        metrics.append("patroni_xlog_received_location {0}".format(
+                        postgres.get('xlog', {}).get('received_location', 0)))
 
         metrics.append("# HELP patroni_server_version Version of Patroni.")
         metrics.append("# TYPE patroni_server_version gauge")
@@ -239,10 +223,7 @@ class RestApiHandler(BaseHTTPRequestHandler):
 
         metrics.append("# HELP patroni_cluster_unlocked Value is 1 if the cluster is unlocked, 0 if locked.")
         metrics.append("# TYPE patroni_cluster_unlocked gauge")
-        if postgres['cluster_unlocked']:
-            metrics.append("patroni_cluster_unlocked 1")
-        else:
-            metrics.append("patroni_cluster_unlocked 0")
+        metrics.append("patroni_cluster_unlocked {0}".format(int(postgres['cluster_unlocked'] == 'true')))
 
         metrics.append("# HELP patroni_timeline Current Postgres timeline number of this node.")
         metrics.append("# TYPE patroni_timeline counter")
