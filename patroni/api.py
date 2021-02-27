cff3fb035952c7debef6c21e50c91d00914aeba9
Mark Mercado <mamercad@gmail.com>
2021-02-27 16:51:59 -0500

    Add xlog replayed timestamp

--- patroni/api.py
+++ patroni/api.py
@@ -234,6 +234,12 @@ class RestApiHandler(BaseHTTPRequestHandler):
         metrics.append("patroni_xlog_replayed_location {0}".format(
                         postgres.get('xlog', {}).get('replayed_location', 0)))
 
+        metrics.append("# HELP patroni_xlog_replayed_timestamp Current timestamp of the replayed"
+                       " Postgres transaction log, 0 if null.")
+        metrics.append("# TYPE patroni_xlog_received_timestamp counter")
+        metrics.append("patroni_xlog_replayed_timestamp {0}".format(
+                        int(postgres.get('xlog', {}).get('replayed_timestamp', 0) is not None)))
+
         metrics.append("# HELP patroni_xlog_paused Value is 1 if the Postgres xlog is paused, 0 otherwise.")
         metrics.append("# TYPE patroni_xlog_paused gauge")
         metrics.append("patroni_xlog_paused {0}".format(
