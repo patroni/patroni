9288ce066b1271ef6b9838c3a584791b8e1ba396
Alexander Kukushkin <cyberdemn@gmail.com>
2021-07-21 08:33:08 +0200

    Reload REST API certificate only on SIGHUP (#2004)
    And make sure that cert number is cached when the RestApiServer object is created.
    
    Close https://github.com/zalando/patroni/issues/2003

--- patroni/__init__.py
+++ patroni/__init__.py
@@ -74,7 +74,7 @@ class Patroni(AbstractPatroniDaemon):
             if local:
                 self.tags = self.get_tags()
                 self.request.reload_config(self.config)
-            if local or self.api.reload_local_certificate():
+            if local or sighup and self.api.reload_local_certificate():
                 self.api.reload_config(self.config['restapi'])
             self.watchdog.reload_config(self.config)
             self.postgresql.reload_config(self.config['postgresql'], sighup)
