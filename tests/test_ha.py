93efa91bbdc5a50cad4f99c4e99286340ab94ce8
Alexander Kukushkin <cyberdemn@gmail.com>
2021-08-19 15:44:37 +0200

    Release 2.1.1 (#2039)
    * Update release notes
    * Bump version
    * Improve unit-test coverage

--- tests/test_ha.py
+++ tests/test_ha.py
@@ -35,8 +35,8 @@ def false(*args, **kwargs):
 
 def get_cluster(initialize, leader, members, failover, sync, cluster_config=None):
     t = datetime.datetime.now().isoformat()
-    history = TimelineHistory(1, '[[1,67197376,"no recovery target specified","' + t + '"]]',
-                              [(1, 67197376, 'no recovery target specified', t)])
+    history = TimelineHistory(1, '[[1,67197376,"no recovery target specified","' + t + '","foo"]]',
+                              [(1, 67197376, 'no recovery target specified', t, 'foo')])
     cluster_config = cluster_config or ClusterConfig(1, {'check_timeline': True}, 1)
     return Cluster(initialize, cluster_config, leader, 10, members, failover, sync, history, None)
 
