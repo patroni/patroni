a01d2ac86dc7e8c38c6630a3fbed24e6725ca127
Robert Cutajar <jocutajar@users.noreply.github.com>
2022-07-14 11:58:19 +0200

    #2021 test HEAD API

--- tests/test_api.py
+++ tests/test_api.py
@@ -285,7 +285,8 @@ class TestRestApiHandler(unittest.TestCase):
 
     def test_do_OPTIONS(self):
         self.assertIsNotNone(MockRestApiServer(RestApiHandler, 'OPTIONS / HTTP/1.0'))
-
+    def test_do_HEAD(self): 
+         self.assertIsNotNone(MockRestApiServer(RestApiHandler, 'HEAD / HTTP/1.0')) 
     def test_do_GET_liveness(self):
         self.assertIsNotNone(MockRestApiServer(RestApiHandler, 'GET /liveness HTTP/1.0'))
 
