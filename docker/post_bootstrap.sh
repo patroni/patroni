2a091d1c18d3e899b21e3250975f36a67770e09a
Peter Pflaeging <peter@pflaeging.net>
2022-08-05 11:50:09 +0200

    next try

--- docker/post_bootstrap.sh
+++ docker/post_bootstrap.sh
@@ -12,12 +12,12 @@ echo Connecting: $connectstring
 
 createdb="SELECT 'CREATE DATABASE "$POSTGRES_DB"' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '"$POSTGRES_DB"')\gexec"
 createuser="SELECT 'CREATE USER "$POSTGRES_USER"' where not exists (select from pg_user where usename = '"$POSTGRES_USER"')\gexec"
-createpasswd="ALTER USER "$POSTGRES_USER" PASSWORD '"$POSTGRES_PASSWORD"';"
+createpasswd="ALTER USER "$POSTGRES_USER" PASSWORD '"$POSTGRES_PASSWD"';"
 giveowner="ALTER DATABASE "$POSTGRES_DB" OWNER TO "$POSTGRES_USER";"
 
-echo $createdb | psql $connectstring
-echo $createuser | psql $connectstring
-echo $createpasswd | psql $connectstring
-echo $giveowner | psql $connectstring
+echo $createdb | psql 
+echo $createuser | psql
+echo $createpasswd | psql 
+echo $giveowner | psql 
 
 exit 0
