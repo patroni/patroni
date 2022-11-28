05d237ec4ac22f40ae1205dd24fb34a8b751c6aa
Alexander Kukushkin <cyberdemn@gmail.com>
2022-11-28 16:24:31 +0100

    Switch from localkube to kind and/or k3d
    The only advantage of localkube was being a low weight.
    Anything else started creating only problems:
    1. It is not properly maintained for many years.
    2. It effectively worked only on Linux, but stopped on modern version
       due to changes in iptables.
    
    Instead, we will use widely adoped tools like kind or k3s.
    The "kind-kind" is the default K8s context (see ~/.kube/config), but it
    could be overriden using `PATRONI_KUBERNETES_CONTEXT` environment
    variable. When executed from GH actions the context is set to k3d-k3d,
    because K3s is much faster to start.

--- features/environment.py
+++ features/environment.py
@@ -479,7 +479,7 @@ class KubernetesController(AbstractDcsController):
         os.environ['PATRONI_KUBERNETES_BYPASS_API_SERVICE'] = 'true'
 
         from patroni.dcs.kubernetes import k8s_client, k8s_config
-        k8s_config.load_kube_config(context='local')
+        k8s_config.load_kube_config(context=os.environ.setdefault('PATRONI_KUBERNETES_CONTEXT', 'kind-kind'))
         self._client = k8s_client
         self._api = self._client.CoreV1Api()
 
