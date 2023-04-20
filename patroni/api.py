patroni/patroni

commit 5421cdce9700e3a3338f9bd41deb4ad57dd49c2b

Resolve some `pyright` issues
This commit fixes these issues:

```
  patroni/api.py:1532:21 - error: Cannot access member "do_handshake" for type "socket"
    Member "do_handshake" is unknown (reportGeneralTypeIssues)
  patroni/api.py:1545:25 - error: Cannot access member "unwrap" for type "socket"
    Member "unwrap" is unknown (reportGeneralTypeIssues)
```

Signed-off-by: Israel Barth Rubio <israel.barth@enterprisedb.com>

patroni/api.py
@@ -16,6 +16,7 @@ import dateutil.parser
 import datetime
 import os
 import socket
+from ssl import SSLSocket
 import sys
 
 from http.server import BaseHTTPRequestHandler, HTTPServer
@@ -1517,7 +1518,7 @@ class RestApiServer(ThreadingMixIn, HTTPServer, Thread):
         if reloading_config:
             self.start()
 
-    def process_request_thread(self, request: socket.socket, client_address: Tuple[str, int]) -> None:
+    def process_request_thread(self, request: Union[socket.socket, SSLSocket], client_address: Tuple[str, int]) -> None:
         """Process a request to the REST API.
 
         Wrapper for :func:`ThreadingMixIn.process_request_thread` that additionally:
@@ -1529,10 +1530,11 @@ class RestApiServer(ThreadingMixIn, HTTPServer, Thread):
         """
         enable_keepalive(request, 10, 3)
         if hasattr(request, 'context'):  # SSLSocket
+            assert type(request) == SSLSocket  # pyright
             request.do_handshake()
         super(RestApiServer, self).process_request_thread(request, client_address)
 
-    def shutdown_request(self, request: socket.socket) -> None:
+    def shutdown_request(self, request: Union[socket.socket, SSLSocket]) -> None:
         """Shut down a request to the REST API.
 
         Wrapper for :func:`HTTPServer.shutdown_request` that additionally:
@@ -1542,6 +1544,7 @@ class RestApiServer(ThreadingMixIn, HTTPServer, Thread):
         """
         if hasattr(request, 'context'):  # SSLSocket
             try:
+                assert type(request) == SSLSocket  # pyright
                 request.unwrap()
             except Exception as e:
                 logger.debug('Failed to shutdown SSL connection: %r', e)

commit cede0119fbcb3393f84724dbbb5db02c9706c1bc

Fix issue in `test_process_request_thread`
The following issue was introduced by `5421cdce9700e3a3338f9bd41deb4ad57dd49c2b`:

```
>           assert type(request) == SSLSocket  # pyright
E           AssertionError

patroni/api.py:1533: AssertionError
```

Signed-off-by: Israel Barth Rubio <israel.barth@enterprisedb.com>

patroni/api.py
@@ -1530,8 +1530,9 @@ class RestApiServer(ThreadingMixIn, HTTPServer, Thread):
         """
         enable_keepalive(request, 10, 3)
         if hasattr(request, 'context'):  # SSLSocket
-            assert type(request) == SSLSocket  # pyright
-            request.do_handshake()
+            # pyright
+            if isinstance(request, SSLSocket):
+                request.do_handshake()
         super(RestApiServer, self).process_request_thread(request, client_address)
 
     def shutdown_request(self, request: Union[socket.socket, SSLSocket]) -> None:
@@ -1544,8 +1545,9 @@ class RestApiServer(ThreadingMixIn, HTTPServer, Thread):
         """
         if hasattr(request, 'context'):  # SSLSocket
             try:
-                assert type(request) == SSLSocket  # pyright
-                request.unwrap()
+                # pyright
+                if isinstance(request, SSLSocket):
+                    request.unwrap()
             except Exception as e:
                 logger.debug('Failed to shutdown SSL connection: %r', e)
         super(RestApiServer, self).shutdown_request(request)