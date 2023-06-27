915cde72a258756331f66c461ca4a165dcf63db3
Mark Pekala <mark.pekala@nutanix.com>
2023-06-27 11:09:09 -0700

    linting errors

--- patroni/__main__.py
+++ patroni/__main__.py
@@ -11,8 +11,6 @@ from .exceptions import ConfigParseError
 from patroni.daemon import AbstractPatroniDaemon, abstract_main, get_base_arg_parser
 from patroni.dcs import Member
 
-from urllib.request import urlopen
-
 if TYPE_CHECKING:  # pragma: no cover
     from .config import Config
 
@@ -68,20 +66,20 @@ class Patroni(AbstractPatroniDaemon):
                 time.sleep(5)
 
     def ensure_unique_name(self) -> None:
-        """A helper function to prevent splitbrain from operator error"""
+        """ A helper function to prevent splitbrain from operator error """
         cluster = self.dcs.get_cluster()
         if not cluster:
             return
         member = cluster.get_member(self.config['name'], False)
         if not member or type(member) != Member:
             return
-        if member.state != 'running':# or member.api_url == member.conn_url:
+        if member.state != 'running' or member.api_url == member.conn_url:
             return
         resp = self.request(member, endpoint="/liveness")
         if resp.status == 200:
             error_str = "Can't start {0}: there is already a node named {0} running".format(self.config['name'])
             logger.exception(error_str)
-            raise ConfigParseError(value=error_str) 
+            raise ConfigParseError(value=error_str)
 
     def get_tags(self) -> Dict[str, Any]:
         return {tag: value for tag, value in self.config.get('tags', {}).items()
