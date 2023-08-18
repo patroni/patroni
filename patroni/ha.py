36dfd0b80eed8273726ed1279278382cfd00414b
Polina Bungina <27892524+hughcapet@users.noreply.github.com>
2023-08-18 15:09:17 +0200

    Update patroni/ha.py

--- patroni/ha.py
+++ patroni/ha.py
@@ -69,7 +69,7 @@ class _MemberStatus(Tags, NamedTuple('_MemberStatus',
 
     @property
     def watchdog_failed(self) -> bool:
-        """indicates that watchdog is required by configuration but not available or failed"""
+        """indicates that watchdog is required by configuration but not available or failed."""
         return self.data.get('watchdog_failed', False)
 
     @classmethod
