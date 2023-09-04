168c08e263a9548b6281fdcf32608153d74f260a
Alexander Kukushkin <cyberdemn@gmail.com>
2023-09-04 15:50:22 +0200

    Update patroni/api.py

--- patroni/api.py
+++ patroni/api.py
@@ -1269,7 +1269,7 @@ class RestApiHandler(BaseHTTPRequestHandler):
 
             row = self.query(stmt.format(postgresql.wal_name, postgresql.lsn_name,
                                          postgresql.wal_flush), retry=retry)[0]
-            result = 
+            result = {
                 'state': postgresql.state,
                 'postmaster_start_time': row[0],
                 'role': 'replica' if row[1] == 0 else 'master',
