patroni/patroni

commit 4c3e0b9382820524239d2aa4d6b95379ef1291db

Remove duplicated code and move logic to underlying `get_any_member`
Calls to `get_any_member` should have either `role` or `member`
argument set, not both.

The patronictl commands `dsn` and `query` were both handling the
mutually exclusivity of `role` and `member`. As they both (and only)
use `get_any_member` function, we pushed that logic down to the
common function.

References: PAT-90.

Signed-off-by: Israel Barth Rubio <israel.barth@enterprisedb.com>

patroni/ctl.py
@@ -474,7 +474,7 @@ def get_all_members(obj: Dict[str, Any], cluster: Cluster,
 
 
 def get_any_member(obj: Dict[str, Any], cluster: Cluster, group: Optional[int],
-                   role: str = 'leader', member: Optional[str] = None) -> Optional[Member]:
+                   role: Optional[str] = None, member: Optional[str] = None) -> Optional[Member]:
     """Get the first found cluster member that has the given *role*.
 
     :param obj: the Patroni configuration.
@@ -484,7 +484,17 @@ def get_any_member(obj: Dict[str, Any], cluster: Cluster, group: Optional[int],
     :param member: if specified, then besides having the given *role*, the cluster member's name should be *member*.
 
     :returns: the first found cluster member that has the given *role*.
+
+    :raises:
+        :class:`PatroniCtlException`: if * both *role* and *member* are provided.
     """
+    if member is not None:
+        if role is not None:
+            raise PatroniCtlException('--role and --member are mutually exclusive options')
+        role = 'any'
+    elif role is None:
+        role = 'leader'
+
     for m in get_all_members(obj, cluster, group, role):
         if member is None or m.name == member:
             return m
@@ -507,7 +517,7 @@ def get_all_members_leader_first(cluster: Cluster) -> Iterator[Member]:
 
 
 def get_cursor(obj: Dict[str, Any], cluster: Cluster, group: Optional[int], connect_parameters: Dict[str, Any],
-               role: str = 'leader', member_name: Optional[str] = None) -> Union['cursor', 'Cursor[Any]', None]:
+               role: Optional[str] = None, member_name: Optional[str] = None) -> Union['cursor', 'Cursor[Any]', None]:
     """Get a cursor object to execute queries against a member that has the given *role* or *member_name*.
 
     .. note::
@@ -706,13 +716,6 @@ def dsn(obj: Dict[str, Any], cluster_name: str, group: Optional[int],
             * both *role* and *member* are provided; or
             * No member matches requested *member* or *role*.
     """
-    if member is not None:
-        if role is not None:
-            raise PatroniCtlException('--role and --member are mutually exclusive options')
-        role = 'any'
-    elif role is None:
-        role = 'leader'
-
     cluster = get_dcs(obj, cluster_name, group).get_cluster()
     m = get_any_member(obj, cluster, group, role=role, member=member)
     if m is None:
@@ -775,18 +778,11 @@ def query(
     :param fmt: the output table printing format. See :func:`print_output` for available options.
 
     :raises:
-        :class:`PatroniCtlException`: if
-            * both *role* and *member* are provided; or
+        :class:`PatroniCtlException`: if:
+            * if * both *role* and *member* are provided; or
             * both *file* and *command* are provided; or
             * neither *file* nor *command* is provided.
     """
-    if member is not None:
-        if role is not None:
-            raise PatroniCtlException('--role and --member are mutually exclusive options')
-        role = 'any'
-    elif role is None:
-        role = 'leader'
-
     if p_file is not None:
         if command is not None:
             raise PatroniCtlException('--file and --command are mutually exclusive options')
@@ -817,7 +813,7 @@ def query(
 
 
 def query_member(obj: Dict[str, Any], cluster: Cluster, group: Optional[int],
-                 cursor: Union['cursor', 'Cursor[Any]', None], member: Optional[str], role: str,
+                 cursor: Union['cursor', 'Cursor[Any]', None], member: Optional[str], role: Optional[str],
                  command: str, connect_parameters: Dict[str, Any]) -> Tuple[List[List[Any]], Optional[List[Any]]]:
     """Execute SQL *command* against a member.
 

commit 51e91a92b494992ff82f5f6f4f291e9a89084868

Fix bug in `patronictl query` command
Previous to this commit `patronictl query` was working only if `-r`
argument was provided to the command. Otherwise it would face issues:

* If neither `-r` nor `-m` were provided:

```
 PGPASSWORD=zalando patronictl -c postgres0.yml query -U postgres -c "SHOW PORT"
2023-09-12 17:45:38	No connection to role=None is available
```

* If only `-m` was provided:

```
$ PGPASSWORD=zalando patronictl -c postgres0.yml query -U postgres -c "SHOW PORT" -m postgresql0
2023-09-12 17:46:15	No connection to member postgresql0 is available
```

This issue was a regression introduced by `4c3e0b9382820524239d2aa4d6b95379ef1291db`
through PR #2687.

Through that PR we decided to move the common logic used to check
mutually exclusiveness of `--role` and `--member` arguments to `get_any_member`
function.

However, previous to that change `role` variable would assume the default value
of `any` in `query` method, before `get_any_member` was called, which was
not the case after the change.

This commit fixes that issue by adding a handler in `get_cursor` function
to `role=None`. As `role` defaulting to `any` is handled in a sub-call to
`get_any_member`, we are apparently safe in `get_cursor` to return the
cursor if `role=None`.

Unit tests were updated accordingly.

References: PAT-204.

Signed-off-by: Israel Barth Rubio <israel.barth@enterprisedb.com>

patroni/ctl.py
@@ -561,9 +561,10 @@ def get_cursor(obj: Dict[str, Any], cluster: Cluster, group: Optional[int], conn
     from . import psycopg
     conn = psycopg.connect(**params)
     cursor = conn.cursor()
-    # If we want ``any`` node we are fine to return the cursor
+    # If we want ``any`` node we are fine to return the cursor. ``None`` is similar to ``any`` at this point, as it's
+    # been dealt with through :func:`get_any_member`.
     # If we want the Patroni leader node, :func:`get_any_member` already checks that for us
-    if role in ('any', 'leader'):
+    if role in (None, 'any', 'leader'):
         return cursor
 
     # If we want something other than ``any`` or ``leader``, then we do not rely only on the DCS information about

tests/test_ctl.py
@@ -75,6 +75,21 @@ class TestCtl(unittest.TestCase):
 
         self.assertIsNotNone(get_cursor({}, get_cluster_initialized_with_leader(), None, {'dbname': 'foo'}, role='any'))
 
+        # Mutually exclusive options
+        with self.assertRaises(PatroniCtlException) as e:
+            get_cursor({}, get_cluster_initialized_with_leader(), None, {'dbname': 'foo'}, member_name='other',
+                       role='replica')
+
+        self.assertEqual(str(e.exception), '--role and --member are mutually exclusive options')
+
+        # Invalid member provided
+        self.assertIsNone(get_cursor({}, get_cluster_initialized_with_leader(), None, {'dbname': 'foo'},
+                                     member_name='invalid'))
+
+        # Valid member provided
+        self.assertIsNotNone(get_cursor({}, get_cluster_initialized_with_leader(), None, {'dbname': 'foo'},
+                                        member_name='other'))
+
     def test_parse_dcs(self):
         assert parse_dcs(None) is None
         assert parse_dcs('localhost') == {'etcd': {'host': 'localhost:2379'}}