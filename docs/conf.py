11d4e408cbe11313781a2f756e6f81ecfb3da495
Alexander Kukushkin <cyberdemn@gmail.com>
2023-09-20 14:52:19 +0200

    Stick with sphinx_rtd_theme
    by default they are using something else

--- docs/conf.py
+++ docs/conf.py
@@ -112,10 +112,10 @@ todo_include_todos = True
 # a list of builtin themes.
 #
 
+html_theme = 'sphinx_rtd_theme'
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 if not on_rtd:  # only import and set the theme if we're building docs locally
     import sphinx_rtd_theme
-    html_theme = 'sphinx_rtd_theme'
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
