bb90feb393aa8dfaf24c28acaf4519137ca7822e
Israel <israel.barth@enterprisedb.com>
2023-10-25 10:01:08 -0300

    Add support for additional parameters on custom bootstrap (#2927)
    Previous to this commit, if a user would ever like to add parameters to the custom bootstrap script call, they would need to configure Patroni like this:
    
    ```
    bootstrap:
      method: custom_method_name
      custom_method_name:
        command: /path/to/my/custom_script --arg1=value1 --arg2=value2 ...
    ```
    
    This commit extends that so we achieve a similar behavior that is seen when using `create_replica_methods`, i.e., we also allow the following syntax:
    
    ```
    bootstrap:
      method: custom_method_name
      custom_method_name:
        command: /path/to/my/custom_script
        arg1: value1
        arg2: value2
    ```
    
    All keys in the mapping which are not recognized by Patroni, will be dealt with as if they were additional named arguments to be passed down to the `command` call.
    
    References: PAT-218.

--- features/backup_restore.py
+++ features/backup_restore.py
@@ -6,6 +6,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--datadir", required=True)
     parser.add_argument("--sourcedir", required=True)
+    parser.add_argument("--test-argument", required=True)
     args, _ = parser.parse_known_args()
 
     shutil.copytree(args.sourcedir, args.datadir)
