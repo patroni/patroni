15b57c5bdc08f2b7a1b27c753029a23f274d0d2a
Polina Bungina <27892524+hughcapet@users.noreply.github.com>
2023-12-20 09:54:04 +0100

    Exclude leader from failover candidates in ctl (#2983)
    Exclude actual leader (not the passed leader argument) from the
    candidates list in the `patronictl failover` prompt.
    Abort `patronictl failover` execution if candidate specified is
    the same as the current cluster leader

--- tests/test_ctl.py
+++ tests/test_ctl.py
@@ -156,7 +156,8 @@ class TestCtl(unittest.TestCase):
         # Target and source are equal
         result = self.runner.invoke(ctl, ['switchover', 'dummy', '--group', '0'], input='leader\nleader\n\ny')
         self.assertEqual(result.exit_code, 1)
-        self.assertIn('Switchover target and source are the same', result.output)
+        self.assertIn("Candidate ['other']", result.output)
+        self.assertIn('Member leader is already the leader of cluster dummy', result.output)
 
         # Candidate is not a member of the cluster
         result = self.runner.invoke(ctl, ['switchover', 'dummy', '--group', '0'], input='leader\nReality\n\ny')
@@ -223,7 +224,10 @@ class TestCtl(unittest.TestCase):
         result = self.runner.invoke(ctl, ['failover', 'dummy'], input='0\n')
         self.assertIn('Failover could be performed only to a specific candidate', result.output)
 
-        cluster = get_cluster_initialized_with_leader(sync=('leader', 'other'))
+        # Candidate is the same as the leader
+        result = self.runner.invoke(ctl, ['failover', 'dummy', '--group', '0'], input='leader\n')
+        self.assertIn("Candidate ['other']", result.output)
+        self.assertIn('Member leader is already the leader of cluster dummy', result.output)
 
         # Temp test to check a fallback to switchover if leader is specified
         with patch('patroni.ctl._do_failover_or_switchover') as failover_func_mock:
