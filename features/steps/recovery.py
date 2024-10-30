059b43d07764eff1905c498522441adce5561083
Polina Bungina <polina.bungina@zalando.de>
2024-10-30 17:16:07 +0100

    Address review feedback

--- features/steps/recovery.py
+++ features/steps/recovery.py
@@ -4,6 +4,6 @@ from behave import step
 
 
 @step('I ensure {name:name} fails to start after a failure')
-def rename_datadir(context, name):
+def spoil_autoconf(context, name):
     with open(os.path.join(context.pctl._processes[name]._data_dir, 'postgresql.auto.conf'), 'w') as f:
         f.write('foo=bar')
