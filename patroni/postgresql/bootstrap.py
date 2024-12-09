f4850c73f12743c4211be49e5a7e9f6c52fc9593
Alexander Kukushkin <cyberdemn@gmail.com>
2024-12-09 11:47:54 +0100

    Cancel long-running jobs on Patroni stop
    Patroni could be doing replica bootstrap and we don't want want
    pg_basebackup/wal-g/pgBackRest/barman or similar keep running.
    
    Besides that, remove data directory on replica bootstrap failure if
    configuration allows.
    
    Close #3224

--- patroni/postgresql/bootstrap.py
+++ patroni/postgresql/bootstrap.py
@@ -314,6 +314,8 @@ class Bootstrap(object):
                     else:
                         logger.error('Error creating replica using method %s: %s exited with code=%s',
                                      replica_method, cmd, ret)
+                        if not method_config.get('keep_data', False):
+                            self._postgresql.remove_data_directory()
                 except Exception:
                     logger.exception('Error creating replica using method %s', replica_method)
                     ret = 1
@@ -341,10 +343,10 @@ class Bootstrap(object):
         ] + user_options
 
         for bbfailures in range(0, maxfailures):
-            if self._postgresql.cancellable.is_cancelled:
-                break
             if not self._postgresql.data_directory_empty():
                 self._postgresql.remove_data_directory()
+            if self._postgresql.cancellable.is_cancelled:
+                break
             try:
                 logger.debug('calling: %r', cmd)
                 ret = self._postgresql.cancellable.call(cmd, env=env)
