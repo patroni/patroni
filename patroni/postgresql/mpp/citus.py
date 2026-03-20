513f8e93ab6c3bb1a6c40ee55c74ad0de485d427
Alexander Kukushkin <cyberdemn@gmail.com>
2026-03-20 17:42:26 +0100

    Postpone CitusHandler.run() (#3571)
    Before #3526 thread was started with the first attempt to sync metadata, where we had guarantees that citus database is prepared (extension exists).
    Early start caused enormous amount of errors.
    We return to old behavior by using `self._ready_to_run = Event()`

--- patroni/postgresql/mpp/citus.py
+++ patroni/postgresql/mpp/citus.py
@@ -403,6 +403,7 @@ class CitusHandler(Citus, AbstractMPPHandler, Thread):
         self._in_flight: Optional[PgDistTask] = None  # Reference to the `PgDistTask` being changed in a transaction
         self._schedule_load_pg_dist_group = True  # Flag that "pg_dist_group" should be queried from the database
         self._condition = Condition()  # protects _pg_dist_group, _tasks, _in_flight, and _schedule_load_pg_dist_group
+        self._ready_to_run = Event()
         self.schedule_cache_rebuild()
         if self.is_coordinator():
             self.start()
@@ -469,6 +470,9 @@ class CitusHandler(Citus, AbstractMPPHandler, Thread):
 
         if not self.is_coordinator():
             return
+
+        # notify run() method that it should start doing its job
+        self._ready_to_run.set()
 
         self.add_task('after_promote', CITUS_COORDINATOR_GROUP_ID, cluster,
                       self._postgresql.name, self._postgresql.connection_string)
@@ -602,6 +606,9 @@ class CitusHandler(Citus, AbstractMPPHandler, Thread):
             task.wakeup()
 
     def run(self) -> None:
+        # we want to postpone "start" until first attempt to sync_meta_data
+        self._ready_to_run.wait()
+
         while True:
             try:
                 with self._condition:
@@ -681,7 +688,7 @@ class CitusHandler(Citus, AbstractMPPHandler, Thread):
         return task if self._add_task(task) else None
 
     def handle_event(self, cluster: Cluster, event: Dict[str, Any]) -> None:
-        if not self.is_coordinator():
+        if not self._ready_to_run.is_set():
             return
 
         worker = cluster.workers.get(event['group'])
