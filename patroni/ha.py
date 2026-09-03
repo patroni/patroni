3c875f3d4bb90e53513faf280233d0bcc9a19270
Polina Bungina <27892524+hughcapet@users.noreply.github.com>
2026-09-03 14:34:40 +0200

    Handle primary_slot_name: null properly (#3710)
    fixes https://github.com/zalando/postgres-operator/issues/3174

--- patroni/ha.py
+++ patroni/ha.py
@@ -2618,7 +2618,7 @@ class Ha(object):
 
         if cluster_params:
             data.update({k: v for k, v in cluster_params.items() if k in RemoteMember.ALLOWED_KEYS})
-            data['no_replication_slot'] = 'primary_slot_name' not in cluster_params
+            data['no_replication_slot'] = not cluster_params.get('primary_slot_name')
             conn_kwargs = member.conn_kwargs() if member else \
                 {k: cluster_params[k] for k in ('host', 'port') if k in cluster_params}
             if conn_kwargs:
